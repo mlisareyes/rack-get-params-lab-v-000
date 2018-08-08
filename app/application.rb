ca8c550e343c48068795e0eb84dd645b87eb55c4
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-08-08 06:48:09 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -22,6 +22,10 @@ class Application
           resp.write "#{item}\n"
         end
       end
+    elsif req.path.match(/add/)
+      add_item = req.params["item"]
+      if @@items.include?(add_item)
+        @@cart << add_item
     else
       resp.write "Path Not Found"
     end
